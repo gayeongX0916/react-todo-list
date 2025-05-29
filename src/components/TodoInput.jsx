a84f61be5c5c217e7390ada06b853fff88f905f5
gayeongX0916 <gayeongx0916@gmail.com>
2025-05-29 10:31:17 +0900

    [feature] 컴포넌트 구조화

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,10 @@
+const TodoInput = () => {
+  return (
+    <>
+      <input placeholder="할 일을 입력해주세요." />
+      <button>추가</button>
+    </>
+  );
+};
+
+export default TodoInput;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,16 @@
+const TodoList = () => {
+  return (
+    <>
+      <section className="todo-item">
+        <div className="todo-title">할 일</div>
+        <li></li>
+      </section>
+      <section className="todo-check">
+        <div className="todo-title">완료된 항목</div>
+        <li></li>
+      </section>
+    </>
+  );
+};
+
+export default TodoList;
